5bc3c4e06e785b81a026040ddab03366d7bfadfb
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-25 17:35:03 -0700

    removed snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,3 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
-
 test('renders learn react link', () => {
   // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
